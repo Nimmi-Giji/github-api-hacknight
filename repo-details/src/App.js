57c68f8c4c04aa07297acb957f132ba6068e4172
gsk <gangaskumar83@gmail.com>
2024-08-03 18:54:29 +0530

    Modified App.js

--- repo-details/src/App.js
+++ repo-details/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios';
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    searchRepos();
+  };
+
+  function searchRepos(){
+    setLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/repos`,
+    }).then(res => {
+      setLoading(false);
+      setRepos(res.data);
+    });
+  }
+
+  function renderRepos(repo){
+    return (
+      <div className="row" key={repo.id}>
+        <h2 className="repo-name">
+          {repo.name}
+        </h2>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <form className = "form">
+        <input className="input"
+        value = {username}
+        placeholder = "Username"
+        onChange = {e => setUsername(e.target.value)} />
+
+        {/* // a form that takes username as input */}
+
+        <button className="button" onClick = {handleSubmit}>{loading ? "Searching..." : "Search"}</button>
+
+        {/* a button that displays search when loading is false, and searching when setLoading(true) is called */}
+
+      </form>
+
+      <div className="results-container">
+        {repos.map(renderRepos)}
+
+        {/* empty array is initialised in useState so the map() function works */}
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
